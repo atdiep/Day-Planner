4c3b69a6740c3e1671c73a514f74ad3c03a173b6
Arnold Diep <tungd@uw.edu>
2019-10-18 11:18:34 -0700

    working on save button on each row and updating rows style based on current time.

--- script.js
+++ script.js
@@ -1,2 +1,29 @@
-var now = moment().format("dddd, MMMM Do, YYYY, h:mm:ss A");
-$('#currentDay').append(now);
+$(document).ready(function () {
+
+    var now = moment().format("dddd, MMMM Do, YYYY, h:mm:ss A");
+    $('#currentDay').append(now);
+
+    function saveRow() {
+        var eventInput = $("textarea");
+        var eventEL = localStorage.getItem("textarea");
+        $(".saveBtn").on("click", function (event) {
+            event.preventDefault();
+
+            var eventEL = $("textarea").val();
+            localStorage.setItem("textarea", eventEL);
+            return saveRow
+        });
+    };
+
+    function updateRowStyle() {
+        var currHour = now.getHours();
+        // Assign each row a time slot.
+        if (currHour = currHour) {
+            document.body.textarea.style.backgroundColor = "white";
+        } else if (currHour ) {
+            document.body.textarea.style.backgroundColor = "grey";
+        } else if (currHour ) {
+            document.body.textarea.style.backgroundColor = "green";
+        }
+    }
+});
